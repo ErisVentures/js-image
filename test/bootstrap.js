515109d6cc288c130ab74c35562aed29a78ae2b8
Patrick Hulce <patrick.hulce@gmail.com>
2017-04-26 22:44:18 -0700

    feat: add basic convolution kernel

--- test/bootstrap.js
+++ test/bootstrap.js
@@ -1,4 +1,7 @@
+const fs = require('fs')
+const memoize = require('lodash/memoize')
 const chai = require('chai')
 chai.use(require('sinon-chai'))
 
 global.expect = chai.expect
+global.fixture = memoize(path => fs.readFileSync(`${__dirname}/fixtures/${path}`))
