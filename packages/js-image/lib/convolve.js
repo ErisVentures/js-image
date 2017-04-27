dc06ead4bc5fc4a04fd294c262cb341b1a6ed27d
Patrick Hulce <patrick.hulce@gmail.com>
2017-04-26 22:44:18 -0700

    feat: add basic convolution kernel

---
+++ packages/js-image/lib/convolve.js
@@ -0,0 +1,68 @@
+function ensureFlatMatrix(matrix) {
+  if (!matrix.length) {
+    throw new Error('Matrix must have length')
+  }
+
+  if (Array.isArray(matrix[0])) {
+    matrix = matrix.reduce((acc, arr) => acc.concat(arr), [])
+  }
+
+  const matrixSize = Math.sqrt(matrix.length)
+  if (matrixSize !== Math.round(matrixSize)) {
+    throw new Error('Matrix must be square')
+  }
+
+  return matrix
+}
+
+
+function convolve(imageData, matrix) {
+  matrix = ensureFlatMatrix(matrix)
+
+  const srcPixels = imageData.data
+  const dstPixels = []
+
+  const imageWidth = imageData.width
+  const imageHeight = imageData.height
+
+  const matrixWidth = Math.sqrt(matrix.length)
+  const matrixHalfWidth = Math.floor(matrixWidth / 2)
+
+  for (var y = 0; y < imageHeight; y++) {
+    for (var x = 0; x < imageWidth; x++) {
+      let r = 0
+      let g = 0
+      let b = 0
+      let a = 0
+
+      let totalWeight = 0
+      for (var matrixY = 0; matrixY < matrixWidth; matrixY++) {
+        for (var matrixX = 0; matrixX < matrixWidth; matrixX++) {
+          const srcX = x + matrixX - matrixHalfWidth
+          const srcY = y + matrixY - matrixHalfWidth
+          if (srcX >= 0 && srcY >= 0 && srcX < imageWidth && srcY < imageHeight) {
+            const srcOffset = (srcY * imageWidth + srcX) * 4
+            const weight = matrix[matrixY * matrixWidth + matrixX]
+            totalWeight += weight
+            r += srcPixels[srcOffset] * weight
+            g += srcPixels[srcOffset + 1] * weight
+            b += srcPixels[srcOffset + 2] * weight
+            a += srcPixels[srcOffset + 3] * weight
+          }
+        }
+      }
+
+      dstPixels.push(
+        Math.round(r / totalWeight),
+        Math.round(g / totalWeight),
+        Math.round(b / totalWeight),
+        Math.round(a / totalWeight)
+      )
+    }
+  }
+
+  debugger
+  return Object.assign({}, imageData, {data: new Uint8Array(dstPixels)})
+}
+
+module.exports = convolve
---
+++ packages/js-image/test/convolve.test.js
@@ -0,0 +1,51 @@
+const fs = require('fs')
+const jpeg = require('jpeg-js')
+const convolve = require('../lib/convolve')
+
+const toPixels = arrs => new Uint8Array(arrs.reduce((acc, arr) => acc.concat(arr), []))
+
+describe('lib/convolve.js', () => {
+  const headshot = jpeg.decode(fixture('headshot.jpg'))
+  const headshotBlur = fixture('headshot-blur.jpg')
+
+  const pixel = value => [value, value, value, 255]
+
+  it('should apply the matrix', () => {
+    const output = convolve({
+      width: 3,
+      height: 3,
+      data: toPixels([
+        pixel(128), pixel(128), pixel(128),
+        pixel(0), pixel(0), pixel(0),
+        pixel(255), pixel(255), pixel(255),
+      ])
+    }, [
+      [0, .2, 0],
+      [.2, .2, .2],
+      [0, .2, 0],
+    ])
+
+    expect(output).to.eql({
+      width: 3,
+      height: 3,
+      data: toPixels([
+        pixel(85), pixel(96), pixel(85),
+        pixel(96), pixel(77), pixel(96),
+        pixel(170), pixel(191), pixel(170),
+      ])
+    })
+  })
+
+  it('should blur', () => {
+    const output = convolve(headshot, [
+      0.0073068827452812644, 0.03274717653776802, 0.05399096651318985, 0.03274717653776802, 0.0073068827452812644,
+      0.03274717653776802, 0.14676266317374237, 0.2419707245191454, 0.14676266317374237, 0.03274717653776802,
+      0.05399096651318985, 0.2419707245191454, 0.3989422804014327, 0.2419707245191454, 0.05399096651318985,
+      0.03274717653776802, 0.14676266317374237, 0.2419707245191454, 0.14676266317374237, 0.03274717653776802,
+      0.0073068827452812644, 0.03274717653776802, 0.05399096651318985, 0.03274717653776802, 0.0073068827452812644
+    ])
+
+    const jpegOutput = jpeg.encode(output, 90)
+    expect(jpegOutput.data).to.eql(headshotBlur)
+  })
+})
