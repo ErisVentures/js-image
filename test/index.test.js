2c40d9e4f30d227c1fab557afe3a809c4eafafc1
Patrick Hulce <patrick.hulce@gmail.com>
2017-04-27 14:40:48 -0700

    refactor: move index to image, test utils

--- test/index.test.js
+++
@@ -1,9 +0,0 @@
-const Image = require('../lib')
-
-describe('index.js', () => {
-  describe('#version', () => {
-    it('should work', () => {
-      expect(Image.version).to.be.ok
-    })
-  })
-})
