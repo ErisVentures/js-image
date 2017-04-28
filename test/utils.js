e9ba024f30cbbf32a6478b489e85d198ce8ee4de
Patrick Hulce <patrick.hulce@gmail.com>
2017-04-27 19:24:46 -0700

    test: easier expectation assertions

--- test/utils.js
+++ test/utils.js
@@ -3,7 +3,21 @@ const memoize = require('lodash/memoize')
 const chai = require('chai')
 chai.use(require('sinon-chai'))
 
+const expect = chai.expect
+const fixturePath = path => `${__dirname}/fixtures/${path}`
+const fixture = memoize(path => fs.readFileSync(fixturePath(path)))
+
 module.exports = {
-  expect: chai.expect,
-  fixture: memoize(path => fs.readFileSync(`${__dirname}/fixtures/${path}`)),
+  expect,
+  fixture,
+  fixturePath,
+  compareToFixture(buffer, path) {
+    fs.writeFileSync(fixturePath(`actual-${path}`), buffer)
+    if (process.env.UPDATE_EXPECTATIONS) {
+      fs.writeFileSync(fixturePath(path), buffer)
+    }
+
+    const expectedData = fixture(path)
+    chai.expect(buffer.length).to.equal(expectedData.length)
+  }
 }
