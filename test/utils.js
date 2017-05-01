1880c2ab0794bc5b163cfff85eca34041b738185
Patrick Hulce <patrick.hulce@gmail.com>
2017-04-30 21:48:29 -0700

    refactor: extract test utility

--- test/utils.js
+++ test/utils.js
@@ -7,21 +7,30 @@ const expect = chai.expect
 const fixturePath = path => `${__dirname}/fixtures/${path}`
 const fixture = memoize(path => fs.readFileSync(fixturePath(path)))
 
+function compareToFixture(buffer, path) {
+  fs.writeFileSync(fixturePath(`actual-${path}`), buffer)
+  if (process.env.UPDATE_EXPECTATIONS) {
+    fs.writeFileSync(fixturePath(path), buffer)
+  }
+
+  const expectedData = fixture(path)
+  if (process.env.LOOSE_COMPARISON === 'true') {
+    expect(buffer.length).to.be.within(expectedData.length - 1000, expectedData.length + 1000)
+  } else {
+    expect(buffer.length).to.equal(expectedData.length)
+  }
+}
+
+function testImage(Image, srcPath, fixturePath, modify) {
+  return modify(Image.from(fixture(srcPath))).toBuffer().then(buffer => {
+    compareToFixture(buffer, fixturePath)
+  })
+}
+
 module.exports = {
   expect,
   fixture,
   fixturePath,
-  compareToFixture(buffer, path) {
-    fs.writeFileSync(fixturePath(`actual-${path}`), buffer)
-    if (process.env.UPDATE_EXPECTATIONS) {
-      fs.writeFileSync(fixturePath(path), buffer)
-    }
-
-    const expectedData = fixture(path)
-    if (process.env.LOOSE_COMPARISON === 'true') {
-      expect(buffer.length).to.be.within(expectedData.length - 1000, expectedData.length + 1000)
-    } else {
-      expect(buffer.length).to.equal(expectedData.length)
-    }
-  }
+  compareToFixture,
+  testImage,
 }
