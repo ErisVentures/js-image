a6459ed6c02ff5eeb04b32e88dbccf4c3f6d2b45
Patrick Hulce <patrick.hulce@gmail.com>
2017-05-21 22:52:29 -0700

    test: add canny test with blur (#4)
    * test: add canny test with blur
    
    * increase timeout

--- test/node-image.test.js
+++ test/node-image.test.js
@@ -71,9 +71,16 @@ describe('NodeImage', () => {
   })
 
   describe('._applyEdges', () => {
-    it('should find sobel edges', () => {
+    it('should find sobel edges', function () {
+      this.timeout(TIMEOUT)
       const modify = img => img.edges()
       return testSkater('skater-edges-sobel.jpg', modify)
+    })
+
+    it('should find canny edges', function () {
+      this.timeout(TIMEOUT)
+      const modify = img => img.edges(NodeImage.CANNY)
+      return testSkater('skater-edges-canny.jpg', modify)
     })
   })
 
