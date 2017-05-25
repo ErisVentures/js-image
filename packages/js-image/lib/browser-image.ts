337dfedc422d47ca35e3efa1c61183b3a110e558
Patrick Hulce <patrick.hulce@gmail.com>
2017-05-24 18:40:56 -0700

    feat: add BrowserImage

---
+++ packages/js-image/lib/browser-image.ts
@@ -0,0 +1,68 @@
+import {BufferLike} from './types'
+import {Image} from './image'
+import {ImageData} from './image-data'
+import {nearestNeighbor} from './transforms/resize'
+import {sobel} from './transforms/sobel'
+import {canny} from './transforms/canny'
+
+export class BrowserImage extends Image {
+  private _image: ImageData
+
+  public constructor(image: ImageData) {
+    super()
+    this._image = image
+  }
+
+  private _applyResize(image: ImageData): ImageData {
+    if (!this._output.resize) {
+      return image
+    }
+
+    return nearestNeighbor(image, this._output.resize)
+  }
+
+  private _applyGreyscale(image: ImageData): ImageData {
+    if (!this._output.greyscale) {
+      return image
+    }
+
+    return ImageData.toGreyscale(image)
+  }
+
+  private _applyEdges(image: ImageData): ImageData {
+    if (!this._output.edges) {
+      return image
+    }
+
+    let edges = sobel(image)
+    if (this._output.edges!.method === Image.CANNY) {
+      edges = canny(edges, undefined)
+    }
+
+    return edges
+  }
+
+  private _applyAll(image: ImageData): Promise<ImageData> {
+    return Promise.resolve(image)
+      .then(image => this._applyGreyscale(image))
+      .then(image => this._applyResize(image))
+      .then(image => this._applyEdges(image))
+  }
+
+  public toImageData(): Promise<ImageData> {
+    return this._applyAll(this._image)
+  }
+
+  public toBuffer(): Promise<Buffer> {
+    return this._applyAll(this._image)
+        .then(imageData => ImageData.toBuffer(imageData))
+  }
+
+  public static from(bufferOrImageData: BufferLike|ImageData): Image {
+    if (ImageData.probablyIs(bufferOrImageData)) {
+      return new BrowserImage(ImageData.normalize(bufferOrImageData))
+    }
+
+    return new BrowserImage(ImageData.from(bufferOrImageData))
+  }
+}
---
+++ packages/js-image/test/browser-image.test.js
@@ -0,0 +1,108 @@
+const jpeg = require('jpeg-js')
+
+const ImageData = require('../lib/image-data').ImageData
+const BrowserImage = require('../lib/browser-image').BrowserImage
+const {expect, fixture, compareToFixture, testImage, TIMEOUT} = require('./utils')
+
+const skater = fixture('skater.jpg')
+const testSkater = (...args) => testImage(BrowserImage, 'skater.jpg', ...args)
+const testYosemite = (...args) => testImage(BrowserImage, 'yosemite-portrait.jpg', ...args)
+const testOpera = (...args) => testImage(BrowserImage, 'opera-landscape.jpg', ...args)
+describe('BrowserImage', () => {
+  describe('._applyResize', () => {
+    it('should resize exactly', () => {
+      const modify = img => img.resize({
+        width: 100,
+        height: 100,
+      })
+
+      return testSkater('browser-skater-square.jpg', modify)
+    })
+  })
+
+  describe('._applyGreyscale', () => {
+    it('should covert to greyscale', () => {
+      const modify = img => img.greyscale()
+      return testSkater('skater-greyscale.jpg', modify)
+    })
+  })
+
+  describe('._applyEdges', () => {
+    it('should find sobel edges', function () {
+      this.timeout(TIMEOUT)
+      const modify = img => img.edges()
+      return testSkater('skater-sobel.jpg', modify)
+    })
+
+    it('should find canny edges', function () {
+      this.timeout(TIMEOUT)
+      const modify = img => img.edges(BrowserImage.CANNY)
+      return testSkater('skater-canny-auto.jpg', modify)
+    })
+  })
+
+  describe('.toImageData', () => {
+    it('should handle RGB image data', () => {
+      const pixels = Buffer.from([
+        0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0,
+      ])
+
+      const imageData = {
+        width: 2,
+        height: 2,
+        channels: 3,
+        format: ImageData.RGB,
+        data: pixels,
+      }
+
+      return BrowserImage.from(imageData).toImageData().then(data => {
+        expect(data).to.eql(imageData)
+      })
+    })
+
+    it('should be fast', function () {
+      this.timeout(TIMEOUT)
+      let promise = Promise.resolve(skater)
+      for (let i = 0; i < 100; i++) {
+        promise = promise.then(image => BrowserImage.from(image).toImageData())
+      }
+
+      return promise.then(imageData => {
+        const decoded = ImageData.normalize(jpeg.decode(skater))
+        expect(imageData.data.length).to.equal(decoded.data.length)
+      })
+    })
+
+    it('should generate a valid image data', () => {
+      const decoded = jpeg.decode(skater)
+      return BrowserImage.from(skater).toImageData().then(imageData => {
+        expect(imageData).to.have.property('width', decoded.width)
+        expect(imageData).to.have.property('height', decoded.height)
+        expect(imageData).to.have.property('data').with.length(decoded.data.length)
+      })
+    })
+  })
+
+  describe('.toBuffer', () => {
+    it('should output the buffer', () => {
+      const image = BrowserImage.from(skater)
+      return image.toBuffer().then(buffer => {
+        expect(buffer).to.be.instanceOf(Buffer)
+        expect(buffer.length).to.be.within(skater.length - 5000, skater.length + 5000)
+      })
+    })
+  })
+
+  describe('#from', () => {
+    it('should return an image from image data', () => {
+      const image = BrowserImage.from(jpeg.decode(skater))
+      expect(image).to.be.instanceOf(BrowserImage)
+    })
+
+    it('should return an image from buffer', () => {
+      const image = BrowserImage.from(skater)
+      expect(image).to.be.instanceOf(BrowserImage)
+    })
+  })
+})
