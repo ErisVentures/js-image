d06b1c252b0afd16bcc9015c55c57cdd8e544c8b
Patrick Hulce <patrick.hulce@gmail.com>
2017-05-24 18:48:29 -0700

    fix: respect resize fit, lint

--- lib/transforms/resize.ts
+++ lib/transforms/resize.ts
@@ -1,31 +1,31 @@
 import {IResizeOptions} from '../types'
 import {ImageData} from '../image-data'
 
-export function nearestNeighbor(imageData: ImageData, options: IResizeOptions) {
+export function nearestNeighbor(imageData: ImageData, options: IResizeOptions): ImageData {
   if (!imageData.width || !imageData.height) {
-    throw new Error('Missing width or height');
+    throw new Error('Missing width or height')
   }
 
-  const targetWidth = options.width!;
-  const targetHeight = options.height!;
-  const scaleFactor = imageData.width / targetWidth;
-  const scaledHeight = Math.floor(imageData.height / scaleFactor);
+  const targetWidth = options.width!
+  const targetHeight = options.height!
+  const scaleFactor = imageData.width / targetWidth
+  const scaledHeight = Math.floor(imageData.height / scaleFactor)
   if (targetHeight !== scaledHeight) {
-    throw new Error('Can only resize exactly');
+    throw new Error('Can only resize exactly')
   }
 
-  const outPixels = new Uint8Array(targetWidth * targetHeight * imageData.channels);
+  const outPixels = new Uint8Array(targetWidth * targetHeight * imageData.channels)
 
   for (let i = 0; i < targetWidth; i++) {
     for (let j = 0; j < targetHeight; j++) {
-      const origX = Math.floor(i * scaleFactor);
-      const origY = Math.floor(j * scaleFactor);
+      const origX = Math.floor(i * scaleFactor)
+      const origY = Math.floor(j * scaleFactor)
 
-      const origPos = (origY * imageData.width + origX) * imageData.channels;
-      const outPos = (j * targetWidth + i) * imageData.channels;
+      const origPos = (origY * imageData.width + origX) * imageData.channels
+      const outPos = (j * targetWidth + i) * imageData.channels
 
       for (let channel = 0; channel < imageData.channels; channel++) {
-        outPixels[outPos + channel] = imageData.data[origPos + channel];
+        outPixels[outPos + channel] = imageData.data[origPos + channel]
       }
     }
   }
@@ -36,5 +36,5 @@ export function nearestNeighbor(imageData: ImageData, options: IResizeOptions) {
     data: outPixels,
     channels: imageData.channels,
     format: imageData.format,
-  };
+  }
 }
