e04761902945b5f38a73b902d6d62d78a998e10d
Patrick Hulce <patrick.hulce@gmail.com>
2017-05-31 20:25:33 -0700

    chore: add bundle minification

--- rollup.config.js
+++ rollup.config.js
@@ -2,6 +2,7 @@ const path = require('path')
 const resolve = require('rollup-plugin-node-resolve')
 const alias = require('rollup-plugin-alias')
 const commonjs = require('rollup-plugin-commonjs')
+const babili = require('rollup-plugin-babili')
 
 const fsshim = path.join(__dirname, 'lib/shims/fs.js')
 const pngshim = path.join(__dirname, 'lib/shims/png-js.js')
@@ -9,7 +10,7 @@ const jpegshim = path.join(__dirname, 'lib/shims/jpeg-js.js')
 
 module.exports = {
   entry: 'lib/browser-index.js',
-  dest: 'dist/bundle.js',
+  dest: `dist/bundle.${process.env.MINIFY ? 'min.' : ''}js`,
   moduleName: '@ouranos/image',
   format: 'umd',
   sourceMap: true,
@@ -22,5 +23,6 @@ module.exports = {
     }),
     resolve({jsnext: true, main: true}),
     commonjs({}),
+    process.env.MINIFY ? babili({sourceMap: true, comments: false}) : {},
   ],
 }
