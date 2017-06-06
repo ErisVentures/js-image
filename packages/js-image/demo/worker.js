36ab77da20d58892bd59951a8a4b9197d188abec
Patrick Hulce <patrick.hulce@gmail.com>
2017-06-05 23:56:22 -0500

    feat(demo): add droppable image functionality (#20)

---
+++ packages/js-image/demo/worker.js
@@ -0,0 +1,43 @@
+importScripts('bundle.min.js')
+
+const {Image, ImageData} = self['@ouranos/image']
+
+function processImage(imageData, options) {
+  const image = Image.from(imageData)
+
+  image
+    .edges()
+    .toImageData()
+    .then(ImageData.toRGBA)
+    .then(imageData => {
+      self.postMessage({
+        type: 'processed',
+        payload: {
+          imageData: {
+            width: imageData.width,
+            height: imageData.height,
+            data: imageData.data,
+          },
+        },
+      }, undefined, [imageData.data])
+    })
+    .catch(err => {
+      console.error(err)
+      self.postMessage({
+        type: 'error',
+        payload: {
+          name: err.name,
+          message: err.message,
+          stack: err.stack,
+        },
+      })
+    })
+}
+
+self.addEventListener('message', message => {
+  if (message.data.type === 'process') {
+    processImage(message.data.payload.data, message.data.payload.options)
+  } else {
+    throw new Error(`Unrecognized message: ${message.data}`)
+  }
+})
