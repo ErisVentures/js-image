9911c80613afc150ec45f4fc763fa566d610db90
Patrick Hulce <patrick.hulce@gmail.com>
2017-06-10 00:22:06 -0700

    feat(resize): add box resize method (#24)
    * feat(resize): add box resize method
    
    * update tests

--- packages/js-image/lib/transforms/resize.ts
+++ packages/js-image/lib/transforms/resize.ts
@@ -45,15 +45,37 @@ export function bilinear(imageData: ImageData, options: IResizeOptions): ImageDa
     throw new Error('Missing width or height')
   }
 
-  const targetWidth = options.width!
-  const targetHeight = options.height!
-  const scaleFactor = imageData.width / targetWidth
+  let targetWidth = options.width!
+  let targetHeight = options.height!
+  let scaleFactor = imageData.width / targetWidth
   const scaledHeight = Math.round(imageData.height / scaleFactor)
   if (targetHeight !== scaledHeight) {
     throw new Error('Can only resize exactly')
   }
 
-  const outPixels = new Uint8Array(targetWidth * targetHeight * imageData.channels)
+  let boxResizeData: ImageData|null = null
+  let boxResizeOptions: IResizeOptions|null = null
+  if (scaleFactor >= 2) {
+    const boxScaleFactor = Math.floor(scaleFactor)
+    if (scaleFactor === boxScaleFactor) {
+      return box(imageData, options)
+    }
+
+    targetWidth = targetWidth * boxScaleFactor
+    targetHeight = targetHeight * boxScaleFactor
+    scaleFactor = imageData.width / targetWidth
+
+    boxResizeOptions = options
+    boxResizeData = {
+      width: targetWidth,
+      height: targetHeight,
+      data: imageData.data,
+      channels: imageData.channels,
+      format: imageData.format,
+    }
+  }
+
+  let outPixels = new Uint8Array(targetWidth * targetHeight * imageData.channels)
 
   for (var i = 0; i < targetWidth; i++) {
     for (var j = 0; j < targetHeight; j++) {
@@ -85,6 +107,61 @@ export function bilinear(imageData: ImageData, options: IResizeOptions): ImageDa
           imageData.data[srcPosD + channel] * weightPosD / totalWeight
         outPixels[outPos + channel] = Math.round(value)
       }
+    }
+  }
+
+  if (boxResizeData && boxResizeOptions) {
+    boxResizeData!.data = outPixels
+    targetWidth = boxResizeOptions!.width!
+    targetHeight = boxResizeOptions!.height!
+    outPixels = box(boxResizeData, boxResizeOptions).data as Uint8Array
+  }
+
+  return {
+    width: targetWidth,
+    height: targetHeight,
+    data: outPixels,
+    channels: imageData.channels,
+    format: imageData.format,
+  }
+}
+
+export function box(imageData: ImageData, options: IResizeOptions): ImageData {
+  if (!options.width || !options.height) {
+    throw new Error('Missing width or height')
+  }
+
+  const targetWidth = options.width!
+  const targetHeight = options.height!
+  const scaleFactor = imageData.width / targetWidth
+  if (scaleFactor <= 1) {
+    throw new Error('Box resize can only shrink images')
+  } else if (targetHeight !== imageData.height / scaleFactor ||
+      Math.floor(scaleFactor) !== scaleFactor) {
+    throw new Error('Can only resize exactly')
+  }
+
+  const outPixels = new Uint8Array(targetWidth * targetHeight * imageData.channels)
+
+  for (let i = 0; i < targetWidth; i++) {
+    for (let j = 0; j < targetHeight; j++) {
+      const origX = Math.floor(i * scaleFactor)
+      const origY = Math.floor(j * scaleFactor)
+
+      const outPos = (j * targetWidth + i) * imageData.channels
+
+      for (let channel = 0; channel < imageData.channels; channel++) {
+        let value = 0
+        for (let dx = 0; dx < scaleFactor; dx++) {
+          for (let dy = 0; dy < scaleFactor; dy++) {
+            const origPos = ((origY + dy) * imageData.width + (origX + dx)) * imageData.channels
+            value += imageData.data[origPos + channel]
+          }
+        }
+
+        outPixels[outPos + channel] = value / (scaleFactor * scaleFactor)
+      }
+
     }
   }
 
