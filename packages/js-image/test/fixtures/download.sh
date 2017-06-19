7e00c3bbb88fcaa2e67c8bbd05ad428082fa1288
Patrick Hulce <patrick.hulce@gmail.com>
2017-06-18 21:30:43 -0700

    fix(edges): use radius weights
    * Fixes an issue where convolution weights were not being used properly
    * Adds a radius option to sobel/canny/sharpness detection

--- packages/js-image/test/fixtures/download.sh
+++ packages/js-image/test/fixtures/download.sh
@@ -2,7 +2,7 @@
 
 cd $(dirname "$0")
 
-VERSION=v2
+VERSION=v3
 S3PATH="https://s3.amazonaws.com/npm-fixtures/@ouranos/image/$VERSION"
 FILES=$(curl "$S3PATH/files.txt")
 for file in $FILES ;
--- packages/js-image/test/transforms/sobel.test.js
+++ packages/js-image/test/transforms/sobel.test.js
@@ -1,15 +1,45 @@
-const sobel = require('../../lib/transforms/sobel').sobel
+const {sobel, generateWeightMatrix} = require('../../lib/transforms/sobel')
 const ImageData = require('../../lib/image-data').ImageData
-const {fixtureDecode, compareToFixture} = require('../utils')
+const {expect, fixtureDecode, compareToFixture} = require('../utils')
 
 describe('#transforms/sobel', () => {
   const skaterPromise = fixtureDecode('skater.jpg').then(ImageData.normalize)
+
+  describe('#generateWeightMatrix', () => {
+    it('should generate a 5x5 y-matrix', () => {
+      expect(generateWeightMatrix(2, false)).to.eql([
+        1, 2, 4, 2, 1,
+        2, 4, 8, 4, 2,
+        0, 0, 0, 0, 0,
+        -2, -4, -8, -4, -2,
+        -1, -2, -4, -2, -1,
+      ])
+    })
+
+    it('should generate a 5x5 x-matrix', () => {
+      expect(generateWeightMatrix(2, true)).to.eql([
+        1, 2, 0, -2, -1,
+        2, 4, 0, -4, -2,
+        4, 8, 0, -8, -4,
+        2, 4, 0, -4, -2,
+        1, 2, 0, -2, -1,
+      ])
+    })
+  })
 
   it('should find edges', () => {
     return skaterPromise.then(skater => {
       const output = sobel(skater)
       const jpegOutput = ImageData.toBuffer(output)
       return compareToFixture(jpegOutput, 'skater-sobel.jpg')
+    })
+  })
+
+  it('should find edges with radius', () => {
+    return skaterPromise.then(imageData => {
+      const output = sobel(imageData, {radius: 3})
+      const jpegOutput = ImageData.toBuffer(output)
+      return compareToFixture(jpegOutput, 'skater-sobel-radius-3.jpg')
     })
   })
 })
