eea676c09c1c8d4e3bfd9e6b98c9e257b5cfee46
Patrick Hulce <patrick.hulce@gmail.com>
2017-06-25 20:00:45 -0700

    feat(analyses): add sharpness heuristics

--- packages/js-image/test/analyses/sharpness.test.js
+++ packages/js-image/test/analyses/sharpness.test.js
@@ -1,30 +1,25 @@
 const sobel = require('../../lib/transforms/sobel').sobel
-const canny = require('../../lib/transforms/canny').canny
 const sharpness = require('../../lib/analyses/sharpness').sharpness
-const ImageData = require('../../lib/image-data').ImageData
-const {expect, fixtureDecode, compareToFixture, buildLinesImageData} = require('../utils')
+const {expect, fixtureDecode, buildLinesImageData} = require('../utils')
 
 describe('#analyses/sharpness', () => {
-  const skaterPromise = fixtureDecode('source-skater.jpg')
+  const infocusPromise = fixtureDecode('source-sydney-in-focus.jpg')
+  const outfocusPromise = fixtureDecode('source-sydney-out-focus.jpg')
 
   it('should compute the sharpness of a sharp horizontal line', () => {
     const originalData = buildLinesImageData(21, 21, [9, 10, 11])
-    const smallData = buildLinesImageData(7, 7, [3])
-    const maskData = sobel(smallData)
+    const edgeData = sobel(originalData)
 
-    const result = sharpness(originalData, maskData, {})
+    const result = sharpness(edgeData)
+    result.percentEdges = Math.round(result.percentEdges * 1000) / 1000
     expect(result).to.eql({
-      width: 7,
-      height: 7,
-      data: new Uint8Array([
-        0, 0, 0, 0, 0, 0, 0,
-        0, 0, 0, 0, 0, 0, 0,
-        0, 255, 255, 255, 255, 255, 0,
-        0, 0, 0, 0, 0, 0, 0,
-        0, 255, 255, 255, 255, 255, 0,
-        0, 0, 0, 0, 0, 0, 0,
-        0, 0, 0, 0, 0, 0, 0,
-      ]),
+      percentEdges: 0.172,
+      lowerQuartile: 255,
+      median: 255,
+      upperQuartile: 255,
+      lowerVentileAverage: 255,
+      average: 255,
+      upperVentileAverage: 255,
     })
   })
 
@@ -38,31 +33,35 @@ describe('#analyses/sharpness', () => {
       {row: 12, value: 64},
       {row: 13, value: 32},
     ])
-    const smallData = buildLinesImageData(7, 7, [3])
-    const maskData = sobel(smallData)
+    const edgeData = sobel(originalData)
 
-    const result = sharpness(originalData, maskData, {})
+    const result = sharpness(edgeData)
+    result.percentEdges = Math.round(result.percentEdges * 1000) / 1000
     expect(result).to.eql({
-      width: 7,
-      height: 7,
-      data: new Uint8Array([
-        0, 0, 0, 0, 0, 0, 0,
-        0, 0, 0, 0, 0, 0, 0,
-        0, 128, 128, 128, 128, 128, 0,
-        0, 0, 0, 0, 0, 0, 0,
-        0, 128, 128, 128, 128, 128, 0,
-        0, 0, 0, 0, 0, 0, 0,
-        0, 0, 0, 0, 0, 0, 0,
-      ]),
+      percentEdges: 0.345,
+      lowerQuartile: 64,
+      median: 96,
+      upperQuartile: 128,
+      lowerVentileAverage: 32,
+      average: 80,
+      upperVentileAverage: 128,
     })
   })
 
   it('should work on an image', () => {
-    return skaterPromise.then(imageData => {
-      const edgeMask = canny(imageData)
-      const result = sharpness(imageData, edgeMask, {})
-      const output = ImageData.toRGBA(ImageData.normalize(result))
-      return compareToFixture(output, 'skater-sharpness-v0.jpg')
-    })
+    return Promise.all([infocusPromise, outfocusPromise])
+      .then(([sharpImageData, blurImageData]) => {
+        const sharpEdges = sobel(sharpImageData)
+        const blurEdges = sobel(blurImageData)
+
+        const sharpResult = sharpness(sharpEdges)
+        const blurResult = sharpness(blurEdges)
+
+        expect(Math.round(sharpResult.percentEdges * 100)).to.equal(4)
+        expect(sharpResult.percentEdges).to.be.greaterThan(blurResult.percentEdges)
+        expect(sharpResult.average).to.be.greaterThan(blurResult.average)
+        expect(sharpResult.median).to.be.greaterThan(blurResult.median)
+        expect(sharpResult.upperVentileAverage).to.be.greaterThan(blurResult.upperVentileAverage)
+      })
   })
 })
