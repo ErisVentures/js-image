f2c5f966641886352bae4dd7edec55f39255709c
Patrick Hulce <patrick.hulce@gmail.com>
2017-07-12 18:19:14 -0700

    chore(rollup): add commonjs plugin to fix bundle

--- packages/js-exif/rollup.config.js
+++ packages/js-exif/rollup.config.js
@@ -1,11 +1,13 @@
 const babel = require('rollup-plugin-babel')
+const resolve = require('rollup-plugin-node-resolve')
+const commonjs = require('rollup-plugin-commonjs')
 
 module.exports = {
   entry: 'lib/index.js',
   sourceMap: true,
   exports: 'named',
   moduleName: '@ouranous/exif',
-  plugins: [babel()],
+  plugins: [babel(), resolve({jsnext: true, main: true}), commonjs()],
   targets: [
     {dest: 'dist/bundle.js', format: 'umd'},
     {dest: 'dist/bundle.cjs.js', format: 'cjs'},
