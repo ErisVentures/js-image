34b1130368a238a64085c9c04117e65e70350df5
Patrick Hulce <patrick.hulce@gmail.com>
2017-07-12 23:32:37 -0700

    chore(rollup): reduce bundle size with debug shim

--- packages/js-exif/rollup.config.js
+++ packages/js-exif/rollup.config.js
@@ -1,13 +1,20 @@
+const path = require('path')
 const babel = require('rollup-plugin-babel')
 const resolve = require('rollup-plugin-node-resolve')
 const commonjs = require('rollup-plugin-commonjs')
+const alias = require('rollup-plugin-alias')
 
 module.exports = {
   entry: 'lib/index.js',
   sourceMap: true,
   exports: 'named',
   moduleName: '@ouranous/exif',
-  plugins: [babel(), resolve({jsnext: true, main: true}), commonjs()],
+  plugins: [
+    babel(),
+    alias({debug: path.join(__dirname, 'lib/debug-shim.js')}),
+    resolve({jsnext: true, main: true}),
+    commonjs(),
+  ],
   targets: [
     {dest: 'dist/bundle.js', format: 'umd'},
     {dest: 'dist/bundle.cjs.js', format: 'cjs'},
