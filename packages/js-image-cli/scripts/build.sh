f4ec874e7763af1ad4941873d4cae7dbeba00dc5
Patrick Hulce <patrick.hulce@gmail.com>
2017-10-17 21:00:42 -0700

    chore(build): add pkg output to zip

--- packages/js-image-cli/scripts/build.sh
+++ packages/js-image-cli/scripts/build.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
 
 TARGET=${PKG_TARGET:-node8-mac}
+rm -fR out/
+mkdir -p out/sharp/build/Release
+mkdir -p out/sharp/vendor/lib
+
 pkg -t $TARGET -o out/image-cli bin/index.js
+cp -R node_modules/sharp/build/Release out/sharp/build/Release
+cp -R node_modules/sharp/vendor/lib out/sharp/vendor/lib
+
+cd out
+tar -czf $TARGET.tar.gz ./*
