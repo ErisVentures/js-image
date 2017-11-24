c02a82bb2769f911971c8145506cef16e04eb229
Patrick Hulce <patrick.hulce@gmail.com>
2017-11-23 20:29:56 -0800

    chore(build): improved error checks (#5)

--- packages/js-image-cli/scripts/build.sh
+++ packages/js-image-cli/scripts/build.sh
@@ -5,9 +5,9 @@ rm -fR out/
 mkdir -p out/sharp/build/Release
 mkdir -p out/sharp/vendor/lib
 
-pkg -t $TARGET -o out/image-cli bin/index.js
-cp -R node_modules/sharp/build/Release/* out/sharp/build/Release/
-cp -R node_modules/sharp/vendor/lib/* out/sharp/vendor/lib/
+pkg -t $TARGET -o out/image-cli bin/index.js || { echo "pkg failed!"; exit 1; }
+cp -R node_modules/sharp/build/Release/* out/sharp/build/Release/ || { echo "sharp not built!"; exit 1; }
+cp -R node_modules/sharp/vendor/lib/* out/sharp/vendor/lib/ || { echo "sharp missing key files! rm -fR node_modules and try again"; exit 1; }
 
 cd out
 tar -czf $TARGET.tar.gz ./*
@@ -19,4 +19,5 @@ cd /tmp/js_image_cli_test
 
 OUTPUT=$(./image-cli 2>&1)
 echo -e "$OUTPUT"
+rm -fR /tmp/js_image_cli_test
 echo $OUTPUT | grep Usage >/dev/null || exit 1
