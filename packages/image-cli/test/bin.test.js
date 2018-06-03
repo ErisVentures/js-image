e8edc290e10c7d46f222d9b9ac73dbfa8b9ffd85
Patrick Hulce <patrick.hulce@gmail.com>
2018-06-03 15:05:00 -0700

    tests(image-cli): hardcode working directory

--- packages/image-cli/test/bin.test.js
+++ packages/image-cli/test/bin.test.js
@@ -3,6 +3,7 @@ const path = require('path')
 const expect = require('chai').expect
 const execa = require('execa')
 
+const CWD = path.join(__dirname, '..')
 const JS_EXE = path.join(__dirname, '../bin/index.js')
 const CONFIG_PATH = path.join(__dirname, 'fixtures/config.json')
 const ANALYSIS_PATH = path.join(__dirname, 'fixtures/actual-skater-analysis.json')
@@ -10,7 +11,7 @@ const ANALYSIS_PATH = path.join(__dirname, 'fixtures/actual-skater-analysis.json
 describe('bin/index.js', () => {
   it('should run from a file config', () => {
     const args = ['-c', CONFIG_PATH]
-    return execa.stdout(JS_EXE, args).then(stdout => {
+    return execa.stdout(JS_EXE, args, {cwd: CWD}).then(stdout => {
       expect(stdout).to.include('has finished')
       expect(stdout).to.include('errored')
 
