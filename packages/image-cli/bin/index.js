c73e330f3b6afcd75adba1cdb7f2aa36c29d6d02
Patrick Hulce <patrick.hulce@gmail.com>
2018-08-05 18:41:43 +0200

    feat(image-cli): add presets support

--- packages/image-cli/bin/index.js
+++ packages/image-cli/bin/index.js
@@ -1,9 +1,13 @@
 #!/usr/bin/env node
 
+const path = require('path')
+const fs = require('fs')
 const yargs = require('yargs')
 const ConfigEntry = require('../lib/config-entry')
 const Runner = require('../lib/runner')
 const Reporters = require('../lib/reporters')
+
+const PRESETS = fs.readdirSync(path.join(__dirname, '../presets')).map(p => p.replace('.json', ''))
 
 const argv = yargs
   .usage('Usage: $0 -c [config]')
@@ -16,8 +20,11 @@ const argv = yargs
     alias: 'r',
     default: 'pretty',
     choices: ['json', 'pretty'],
-  })
-  .argv
+  }).argv
+
+if (PRESETS.includes(argv.config)) {
+  argv.config = path.join(__dirname, `../presets/${argv.config}.json`)
+}
 
 const configEntries = ConfigEntry.readAllFrom(argv.config)
 const reporter = Reporters.from(argv)
