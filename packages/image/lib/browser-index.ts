e026ebb2e4e96e09db6b784ba1d6074e5fc52fb9
Patrick Hulce <patrick.hulce@gmail.com>
2018-08-23 08:38:47 -0700

    fix(image): shim buffer in browser

--- packages/image/lib/browser-index.ts
+++ packages/image/lib/browser-index.ts
@@ -1,3 +1,4 @@
+import './shims/buffer'
 export {ImageData} from './image-data'
 export {BrowserImage as Image} from './browser-image'
 export {hammingDistance} from './analyses/hash'
---
+++ packages/image/lib/shims/buffer.ts
@@ -0,0 +1,3 @@
+// tslint:disable
+// @ts-ignore
+self.Buffer = require('buffer/').Buffer
