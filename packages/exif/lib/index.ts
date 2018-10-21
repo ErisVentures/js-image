8612769b11049be5fa28764e4adff9c2d3a96251
Patrick Hulce <patrick.hulce@gmail.com>
2018-10-20 23:28:26 -0500

    feat(exif): export types

--- packages/exif/lib/index.ts
+++ packages/exif/lib/index.ts
@@ -29,3 +29,5 @@ export function parse(bufferOrDecoder: IBufferLike | IDecoder): any {
 }
 
 export {normalizeMetadata, TIFFDecoder, JPEGDecoder, TIFFEncoder}
+
+export {IGenericMetadata, INormalizedMetadata, IParsedLens, IFDTagName} from './utils/types'
