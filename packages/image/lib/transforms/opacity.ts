2bd48d008e6c9a2309279952802bf8b26df428d9
Patrick Hulce <patrick.hulce@gmail.com>
2018-11-09 15:41:24 -0600

    fix(image): respect existing alpha channel

--- packages/image/lib/transforms/opacity.ts
+++ packages/image/lib/transforms/opacity.ts
@@ -1,4 +1,5 @@
 import {IAnnotatedImageData, ImageData} from '../image-data'
+import {Colorspace} from '../types'
 
 export function opacity(
   background: IAnnotatedImageData,
@@ -11,14 +12,43 @@ export function opacity(
   if (background.data.length !== foreground.data.length) throw new Error('Sizes must match')
   if (!isUint8) throw new Error('Must be Uint8Array')
 
-  const alphaForeground = opacity
-  const alphaBackground = 1 - opacity
-  // TODO: take alpha channel into account
   const newData = new Uint8Array(background.data.length)
-  for (let i = 0; i < background.data.length; i++) {
-    newData[i] = ImageData.clip(
-      background.data[i] * alphaBackground + foreground.data[i] * alphaForeground,
-    )
+  if (background.colorspace === Colorspace.RGBA) {
+    for (let x = 0; x < background.width; x++) {
+      for (let y = 0; y < background.height; y++) {
+        const index = ImageData.indexFor(background, x, y)
+        const opacityForegroundSrc = foreground.data[index + 3] / 255
+        const opacityForeground = opacity * opacityForegroundSrc
+        const opacityBackground = background.data[index + 3] / 255
+        if (opacityBackground !== 1) throw new Error('Cannot handle transparent backgrounds')
+
+        const foregroundMultiplier = opacityForeground
+        const backgroundMultiplier = 1 - opacityForeground
+
+        newData[index] = ImageData.clip(
+          background.data[index] * backgroundMultiplier +
+            foreground.data[index] * foregroundMultiplier,
+        )
+        newData[index + 1] = ImageData.clip(
+          background.data[index + 1] * backgroundMultiplier +
+            foreground.data[index + 1] * foregroundMultiplier,
+        )
+        newData[index + 2] = ImageData.clip(
+          background.data[index + 2] * backgroundMultiplier +
+            foreground.data[index + 2] * foregroundMultiplier,
+        )
+        newData[index + 3] = 255
+      }
+    }
+  } else {
+    const foregroundMultiplier = opacity
+    const backgroundMultiplier = 1 - opacity
+
+    for (let i = 0; i < background.data.length; i++) {
+      newData[i] = ImageData.clip(
+        background.data[i] * backgroundMultiplier + foreground.data[i] * foregroundMultiplier,
+      )
+    }
   }
 
   return {...background, data: newData}
--- packages/image/test/transforms/opacity.test.js
+++ packages/image/test/transforms/opacity.test.js
@@ -38,4 +38,78 @@ describe('#transforms/opacity', () => {
       ]),
     })
   })
+
+  it('should respect existing alpha channel on solid background', () => {
+    const background = {
+      width: 2,
+      height: 2,
+      channels: 4,
+      colorspace: 'rgba',
+      data: new Uint8Array([
+        255, 255, 255, 255, 255, 255, 255, 255,
+        255, 255, 255, 255, 255, 255, 255, 255
+      ]),
+    }
+
+    const foreground = {
+      width: 2,
+      height: 2,
+      channels: 4,
+      colorspace: 'rgba',
+      data: new Uint8Array([
+        0, 0, 0, 128, 0, 0, 0, 255,
+        0, 0, 0, 64, 0, 0, 0, 8
+      ]),
+    }
+
+    const result = opacity(background, foreground, 1)
+
+    expect(result).to.eql({
+      width: 2,
+      height: 2,
+      channels: 4,
+      colorspace: 'rgba',
+      data: new Uint8Array([
+        127, 127, 127, 255, 0, 0, 0, 255,
+        191, 191, 191, 255, 247, 247, 247, 255
+      ]),
+    })
+  })
+
+  it.skip('should respect existing alpha channel on semi-transparent background', () => {
+    const background = {
+      width: 2,
+      height: 2,
+      channels: 4,
+      colorspace: 'rgba',
+      data: new Uint8Array([
+        255, 255, 255, 128, 255, 255, 255, 128,
+        255, 255, 255, 128, 255, 255, 255, 128
+      ]),
+    }
+
+    const foreground = {
+      width: 2,
+      height: 2,
+      channels: 4,
+      colorspace: 'rgba',
+      data: new Uint8Array([
+        0, 0, 0, 128, 0, 0, 0, 255,
+        0, 0, 0, 64, 0, 0, 0, 8
+      ]),
+    }
+
+    const result = opacity(background, foreground, 1)
+
+    expect(result).to.eql({
+      width: 2,
+      height: 2,
+      channels: 4,
+      colorspace: 'rgba',
+      data: new Uint8Array([
+        127, 127, 127, 255, 0, 0, 0, 255,
+        191, 191, 191, 255, 247, 247, 247, 255
+      ]),
+    })
+  })
 })
