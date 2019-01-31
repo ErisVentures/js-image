d592bf7f51b2bd58bc1b61f49de51694557ee852
Patrick Hulce <patrick.hulce@gmail.com>
2019-01-30 22:28:54 -0600

    feat(exif): add XMPDecoder

---
+++ packages/exif/lib/decoder/xmp-decoder.ts
@@ -0,0 +1,85 @@
+import {
+  IBufferLike,
+  IGenericMetadata,
+  IIFDTagDefinition,
+  IFDTagName,
+} from '../utils/types'
+import {tags} from '../utils/tags'
+
+const EXIF_ATTR_GLOBAL_REGEX = /(exif|tiff):([0-9a-z]+?)="(.*?)"/gim
+const EXIF_ATTR_REGEX = /f:([0-9a-z]+?)="(.*?)"/i
+
+function isSimpleNumber(s: string): boolean {
+  return /^(([1-9]\d*)|0)$/.test(s)
+}
+
+function isComplexNumber(s: string): boolean {
+  return /^[1-9]\d*\/[1-9]\d*$/.test(s)
+}
+
+export class XMPDecoder {
+  private static _tagsByLowerCaseKey: Record<string, IIFDTagDefinition>
+
+  private readonly _text: string
+
+  public constructor(buffer: IBufferLike) {
+    this._text = buffer.toString()
+    this._precomputeIfdTags()
+  }
+
+  private _precomputeIfdTags(): void {
+    if (XMPDecoder._tagsByLowerCaseKey) return
+    const tagsByLowerCaseKey: Record<string, IIFDTagDefinition> = {}
+
+    for (const tagName of Object.keys(tags)) {
+      tagsByLowerCaseKey[tagName.toLowerCase()] = tags[tagName as IFDTagName]
+    }
+
+    XMPDecoder._tagsByLowerCaseKey = tagsByLowerCaseKey
+  }
+
+  private _processRdfDescription(
+    attributes: Record<string, string>,
+    genericMetadata: IGenericMetadata,
+  ): void {
+    for (const key of Object.keys(attributes)) {
+      if (!key.startsWith('exif:') && !key.startsWith('tiff:')) continue
+      const exifLowercaseTagName = key.slice(5)
+      const ifdDefinition = XMPDecoder._tagsByLowerCaseKey[exifLowercaseTagName]
+      if (!ifdDefinition) continue
+
+      const value = attributes[key]
+      let realValue: number|string|undefined
+      if (isSimpleNumber(value)) {
+        realValue = Number(value)
+      } else if (isComplexNumber(value)) {
+        const [numerator, denominator] = value.split('/')
+        realValue = Number(numerator) / Number(denominator)
+      } else {
+        realValue = value
+      }
+
+      genericMetadata[ifdDefinition.name] = realValue
+    }
+  }
+
+  public extractMetadata(): IGenericMetadata {
+    const metadata: IGenericMetadata = {}
+    const attributes: Record<string, string> = {}
+    const matches = this._text.match(EXIF_ATTR_GLOBAL_REGEX)
+
+    for (const match of (matches || [])) {
+      // @ts-ignore - guaranteed to match from above
+      const [_, key, value] = match.match(EXIF_ATTR_REGEX)
+      attributes[`exif:${key.toLowerCase()}`] = value
+    }
+
+    this._processRdfDescription(attributes, metadata)
+
+    return metadata
+  }
+
+  public static isXMP(buffer: IBufferLike): boolean {
+    return buffer[0] === '<'.charCodeAt(0)
+  }
+}
---
+++ packages/exif/test/decoder/xmp-decoder.test.js
@@ -0,0 +1,47 @@
+const {expect, fixture} = require('../utils')
+const XMPDecoder = require('../../dist/decoder/xmp-decoder').XMPDecoder
+
+const xmpFile = fixture('d4s.xmp')
+
+describe('lib/decoder/xmp-decoder.js', () => {
+  describe('.extractMetadata', () => {
+    it('should work', () => {
+      const metadata = new XMPDecoder(xmpFile).extractMetadata()
+      expect(metadata).to.eql({
+        ApertureValue: 2.970854,
+        Contrast: 0,
+        CustomRendered: 0,
+        DateTimeOriginal: '2014-04-01T09:23:43.29',
+        DigitalZoomRatio: 1,
+        EXIFVersion: '0221',
+        ExposureMode: 0,
+        ExposureProgram: 3,
+        ExposureTime: 0.0125,
+        FNumber: 2.8,
+        FileSource: 3,
+        FocalLength: 70,
+        FocalPlaneResolutionUnit: 3,
+        FocalPlaneXResolution: 1368.8888854980469,
+        FocalPlaneYResolution: 1368.8888854980469,
+        GainControl: 0,
+        ImageLength: 3280,
+        ImageWidth: 4928,
+        LightSource: 0,
+        Make: 'NIKON CORPORATION',
+        MaxApertureValue: 3,
+        MeteringMode: 5,
+        Model: 'NIKON D4S',
+        Orientation: 1,
+        Saturation: 0,
+        SceneCaptureType: 0,
+        SceneType: 1,
+        SensingMethod: 2,
+        SensitivityType: 2,
+        Sharpness: 0,
+        ShutterSpeedValue: 6.321928,
+        SubjectDistanceRange: 0,
+        WhiteBalance: 0,
+      })
+    })
+  })
+})
