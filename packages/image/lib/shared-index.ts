b8b08c09d1f2d26d4a5a9363866e43e340bf6c0f
Patrick Hulce <patrick.hulce@gmail.com>
2019-11-02 20:58:57 -0500

    feat(image): expose subsetDistance

--- packages/image/lib/shared-index.ts
+++ packages/image/lib/shared-index.ts
@@ -1,5 +1,5 @@
 import * as _types from './types'
 export {ImageData} from './image-data'
-export {hammingDistance} from './analyses/hash'
+export {hammingDistance, subsetDistance} from './analyses/hash'
 export {ImageFormat, ImageResizeFit, ImageResizeMethod, EdgeMethod, HashMethod} from './types'
 export const types = _types
