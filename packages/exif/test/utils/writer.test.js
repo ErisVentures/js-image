29ee5cd26ef6926069bfc74b9ff63ac4113e8d0a
Patrick Hulce <patrick.hulce@gmail.com>
2019-12-05 14:17:52 -0600

    feat(exif): splice range method in writer

--- packages/exif/test/utils/writer.test.js
+++ packages/exif/test/utils/writer.test.js
@@ -20,5 +20,39 @@ describe('Writer', () => {
       writer.write(0xff, 4)
       expect(writer.toBuffer()).toEqual(toArray([0xff, 0xcc, 0, 0, 0, 0xff]))
     })
+
+    it('should write after a pre-filled buffer', () => {
+      const originalBuffer = Buffer.from([0x11, 0x11])
+      const writer = new Writer(originalBuffer)
+      writer.setEndianess(Endian.Big)
+      writer.write(0xff, 4)
+      expect(writer.toBuffer()).toEqual(toArray([0x11, 0x11, 0, 0, 0, 0xff]))
+      expect(originalBuffer).toEqual(Buffer.from([0x11, 0x11]))
+    })
+
+    it('should directly overwrite storage', () => {
+      const originalBuffer = Buffer.from([0x11, 0x11, 0x11, 0x11, 0x11, 0x11])
+      const writer = new Writer(originalBuffer, {dangerouslyAvoidCopy: true})
+      writer.setEndianess(Endian.Big)
+      writer.write(0xff, 4)
+      expect(writer.toBuffer()).toEqual(toArray([0, 0, 0, 0xff, 0x11, 0x11]))
+      expect(originalBuffer).toEqual(Buffer.from([0, 0, 0, 0xff, 0x11, 0x11]));
+    })
+  })
+
+  describe('#spliceBufferRange', () => {
+    it('should replace the range with a new buffer', () => {
+      const start = Buffer.from([0xff, 0xff, 0xff, 0xff])
+      const replacement = Buffer.from([0x11])
+      expect(Writer.spliceBufferRange(start, replacement, 0, 1)).toEqual(
+        Buffer.from([0x11, 0xff, 0xff, 0xff]),
+      )
+      expect(Writer.spliceBufferRange(start, replacement, 1, 2)).toEqual(
+        Buffer.from([0xff, 0x11, 0xff, 0xff]),
+      )
+      expect(Writer.spliceBufferRange(start, replacement, 1, 3)).toEqual(
+        Buffer.from([0xff, 0x11, 0xff]),
+      )
+    })
   })
 })
