d2151af3d64e289a150933b8ee4b8f700eb236dd
Patrick Hulce <patrick.hulce@gmail.com>
2019-12-05 14:17:25 -0600

    tests(exif): add more date normalization tests

--- packages/exif/test/metadata/normalize.test.js
+++ packages/exif/test/metadata/normalize.test.js
@@ -14,7 +14,7 @@ describe('lib/metadata/normalize.js', () => {
       expect(result).toMatchObject({width: 4000, height: 3000})
     })
 
-    it('should handle dates with care', () => {
+    it('should handle ISO-like dates', () => {
       const metadata = {
         CreateDate: '2014-04-01T09:23:43.29',
         ModifyDate: '2014-04-01T09:23:43.29-01:00',
@@ -23,6 +23,18 @@ describe('lib/metadata/normalize.js', () => {
       expect(result).toMatchObject({
         createdAt: new Date('2014-04-01T09:23:43.290Z'),
         modifiedAt: new Date('2014-04-01T10:23:43.290Z'),
+      })
+    })
+
+    it('should handle colon spaced dates', () => {
+      const metadata = {
+        CreateDate: '2014:04:01 09:23:43',
+        ModifyDate: '2014:04:01 10:23:43',
+      }
+      const result = normalizeMetadata(metadata)
+      expect(result).toMatchObject({
+        createdAt: new Date('2014-04-01T09:23:43.000Z'),
+        modifiedAt: new Date('2014-04-01T10:23:43.000Z'),
       })
     })
   })
