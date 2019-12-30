3e6497b6dd8f5379c021188c23f9debf002602f8
Patrick Hulce <patrick.hulce@gmail.com>
2019-12-30 16:04:39 -0600

    chore: move to node 10 on travis
    BREAKING CHANGE: binary assets now prefixed by node10 not node8

--- packages/image-cli/scripts/build.sh
+++ packages/image-cli/scripts/build.sh
@@ -5,7 +5,7 @@ IMAGE_CLI_ROOT="$DIRNAME/.."
 TARGET=${PKG_TARGET:-node10-mac}
 rm -rf out/
 mkdir -p out/@tensorflow/tfjs-node/deps
-mkdir -p out/@tensorflow/tfjs-node/lib/napi-v3
+mkdir -p out/@tensorflow/tfjs-node/lib
 mkdir -p out/sharp/build/Release
 mkdir -p out/sharp/vendor/lib
 
@@ -13,7 +13,7 @@ pkg -t $TARGET -o out/image-cli . || { echo "pkg failed!"; exit 1; }
 cp -R ../../node_modules/sharp/build/Release/* out/sharp/build/Release/ || { echo "sharp not built!"; exit 1; }
 cp -R ../../node_modules/sharp/vendor/lib/* out/sharp/vendor/lib/ || { echo "sharp missing key files! rm -fR node_modules and try again"; exit 1; }
 cp -RL ../../node_modules/@tensorflow/tfjs-node/deps/* out/@tensorflow/tfjs-node/deps || { echo "tfjs not built!"; exit 1; }
-cp -RL ../../node_modules/@tensorflow/tfjs-node/lib/napi-v3/* out/@tensorflow/tfjs-node/lib/napi-v3 || { echo "tfjs not built!"; exit 1; }
+cp -RL ../../node_modules/@tensorflow/tfjs-node/lib/* out/@tensorflow/tfjs-node/lib || { echo "tfjs not built!"; exit 1; }
 
 cd out
 tar -czf $TARGET.tar.gz ./*
