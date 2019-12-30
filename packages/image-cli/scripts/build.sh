fd6256dba33a6baa3f252b7765253661c7d55c81
Patrick Hulce <patrick.hulce@gmail.com>
2019-12-30 15:12:05 -0600

    chore(image-cli): update build process for tfjs@1.5.0

--- packages/image-cli/scripts/build.sh
+++ packages/image-cli/scripts/build.sh
@@ -4,16 +4,16 @@ DIRNAME="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 IMAGE_CLI_ROOT="$DIRNAME/.."
 TARGET=${PKG_TARGET:-node10-mac}
 rm -rf out/
-mkdir -p out/@tensorflow/tfjs-node/build/Release
+mkdir -p out/@tensorflow/tfjs-node/deps
+mkdir -p out/@tensorflow/tfjs-node/lib/napi-v3
 mkdir -p out/sharp/build/Release
 mkdir -p out/sharp/vendor/lib
 
 pkg -t $TARGET -o out/image-cli . || { echo "pkg failed!"; exit 1; }
 cp -R ../../node_modules/sharp/build/Release/* out/sharp/build/Release/ || { echo "sharp not built!"; exit 1; }
 cp -R ../../node_modules/sharp/vendor/lib/* out/sharp/vendor/lib/ || { echo "sharp missing key files! rm -fR node_modules and try again"; exit 1; }
-cp -RL ../../node_modules/@tensorflow/tfjs-node/build/Release/* out/@tensorflow/tfjs-node/build/Release || { echo "tfjs not built!"; exit 1; }
-
-rm -rf out/@tensorflow/tfjs-node/build/Release/.deps # Clear out the references to old symlinked deps
+cp -RL ../../node_modules/@tensorflow/tfjs-node/deps/* out/@tensorflow/tfjs-node/deps || { echo "tfjs not built!"; exit 1; }
+cp -RL ../../node_modules/@tensorflow/tfjs-node/lib/napi-v3/* out/@tensorflow/tfjs-node/lib/napi-v3 || { echo "tfjs not built!"; exit 1; }
 
 cd out
 tar -czf $TARGET.tar.gz ./*
