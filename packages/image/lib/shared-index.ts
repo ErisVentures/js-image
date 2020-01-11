cef8a51e43ed40de71fa7d9bf8f9a0d408888296
Patrick Hulce <patrick.hulce@gmail.com>
2020-01-11 14:34:43 -0600

    feat(image): expose hash functions on index

--- packages/image/lib/shared-index.ts
+++ packages/image/lib/shared-index.ts
@@ -1,5 +1,7 @@
 import * as _types from './types'
+import * as _hashes from './analyses/hash'
 export {ImageData} from './image-data'
 export {hammingDistance, subsetDistance} from './analyses/hash'
 export {ImageFormat, ImageResizeFit, ImageResizeMethod, EdgeMethod, HashMethod} from './types'
 export const types = _types
+export const hashes = _hashes
