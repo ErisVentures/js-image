71a8e9cdbcfed439cf493bf299c8565e9a51203f
Patrick Hulce <patrick.hulce@gmail.com>
2020-02-03 17:17:50 -0600

    chore: update publish script to use headers

--- scripts/add-asset-to-github.sh
+++ scripts/add-asset-to-github.sh
@@ -24,12 +24,12 @@ if [[ -z "$TRAVIS_TAG" ]]; then
   exit 0
 fi
 
-GITHUB_URL="https://api.github.com/repos/ErisVentures/js-image/releases/tags/$TRAVIS_TAG?access_token=$GH_TOKEN"
+GITHUB_URL="https://api.github.com/repos/ErisVentures/js-image/releases/tags/$TRAVIS_TAG"
 echo "Determining release ID for $TRAVIS_TAG..."
-RELEASE_ID=$(curl "$GITHUB_URL" | node -e 'console.log(JSON.parse(fs.readFileSync(0, "utf8")).id)')
+RELEASE_ID=$(curl -u "patrickhulce:$GH_TOKEN" "$GITHUB_URL" | node -e "console.log(JSON.parse(fs.readFileSync(0, \"utf8\")).id)")
 echo "$TRAVIS_TAG is release $RELEASE_ID"
 
 BASENAME_TO_PUBLISH="image-cli-$(basename "$ASSET_PATH")"
 echo "Publishing $ASSET_PATH as $BASENAME_TO_PUBLISH..."
-GITHUB_URL="https://uploads.github.com/repos/ErisVentures/js-image/releases/$RELEASE_ID/assets?name=$BASENAME_TO_PUBLISH&access_token=$GH_TOKEN"
-curl --data-binary @"$ASSET_PATH" -H 'Content-Type: application/octet-stream' "$GITHUB_URL"
+GITHUB_URL="https://uploads.github.com/repos/ErisVentures/js-image/releases/$RELEASE_ID/assets?name=$BASENAME_TO_PUBLISH"
+curl -u "patrickhulce:$GH_TOKEN" --data-binary @"$ASSET_PATH" -H 'Content-Type: application/octet-stream' "$GITHUB_URL"
