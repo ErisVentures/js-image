de1eb51d96eaad3c1169f1bc42ab3a3b46039ddc
Patrick Hulce <patrick.hulce@gmail.com>
2020-03-20 13:19:52 -0500

    feat(image): expose instrumentation

--- packages/image/lib/shared-index.ts
+++ packages/image/lib/shared-index.ts
@@ -1,5 +1,6 @@
 import * as _types from './types'
 import * as _hashes from './analyses/hash'
+export {instrumentation} from './instrumentation'
 export {ImageData} from './image-data'
 export {hammingDistance, subsetDistance} from './analyses/hash'
 export {ImageFormat, ImageResizeFit, ImageResizeMethod, EdgeMethod, HashMethod} from './types'
