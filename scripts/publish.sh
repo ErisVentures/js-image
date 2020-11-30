331d7227aadb3f696f4215a0b56cb4598703b798
Patrick Hulce <patrick.hulce@gmail.com>
2020-11-29 22:36:19 -0600

    chore(ci): split publish to separate build step

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -10,11 +10,11 @@ export TRAVIS_NODE_VERSION=v8
 export TRAVIS_BRANCH="$GITHUB_EXTRA__BRANCH"
 
 PRERELEASE_FLAGS="--prerelease"
-if git log "$GITHUB_SHA" | grep 'OFFICIAL RELEASE'; then
+if git log -n 1 "$GITHUB_SHA" | grep 'OFFICIAL RELEASE'; then
   PRERELEASE_FLAGS=""
 fi
 
-npm install -g @patrickhulce/scripts
+npm install -g @patrickhulce/scripts lerna@3.x
 git checkout -f master
 git status
 hulk npm-publish --lerna --yes $PRERELEASE_FLAGS
