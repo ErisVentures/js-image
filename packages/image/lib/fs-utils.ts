f0dc44ff7dff54e4173ecca2645c26c198876d81
Patrick Hulce <patrick.hulce@gmail.com>
2021-02-07 22:53:32 -0600

    fix(image): use void not {}

--- packages/image/lib/fs-utils.ts
+++ packages/image/lib/fs-utils.ts
@@ -1,8 +1,8 @@
 import {BufferLike} from './types'
 import {writeFile} from 'fs'
 
-export function writeFileAsync(file: string, buffer: BufferLike): Promise<{}> {
-  return new Promise<{}>((resolve, reject) => {
+export function writeFileAsync(file: string, buffer: BufferLike): Promise<void> {
+  return new Promise<void>((resolve, reject) => {
     writeFile(file, Buffer.from(buffer as any[]), err => {
       if (err) {
         reject(err)
